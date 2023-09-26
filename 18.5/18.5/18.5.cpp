c7b23fa2faa695981e51a907ecce32b0e77d4ddb
TimurV2 <sabzerov303@gmail.com>
2023-09-26 20:36:31 +0300

    redoing 18.5

--- 18.5/18.5/18.5.cpp
+++ 18.5/18.5/18.5.cpp
@@ -6,5 +6,9 @@ using namespace std;
 
 int main()
 {
+    int num_players;
+    cout << "How many players do you want to add? -> ";
+    cin >> num_players;
+
     
 }
--- 18.5/18.5/player_class.h
+++ 18.5/18.5/player_class.h
@@ -6,28 +6,30 @@ class player_class
 {
 private:
 
-	string name;
-	int score;
+	int size_arr;
+	//string *arr_name_plr;
+	int* arr_score_plr;
 
 public:
-
-	player_class(string pl_name, int pl_score) 
+	
+	player_class(int num_of_players) // Конструктор создания экземлпяра класса с массивами имен и счета игроков
 	{
-		this->score = pl_score;
-		this->name = pl_name;
+		this->size_arr = num_of_players;
+		//arr_name_plr = new string[num_of_players];
+		arr_score_plr = new int[num_of_players];
+		player_data_input(num_of_players);
 	}
 
-	int get_score()
+	void player_data_input(int num_of_players) // Метод класса для ввода данных в массивы имен и счета игрков
 	{
-		return this->score;
+		for (int x = 0; x < num_of_players; x++)
+		{
+			for (int y = 0; y < num_of_players; y++)
+			{
+
+			}
+		}
 	}
 
-	string get_name() 
-	{
-		return this->name;
-	}
-
-	void print_score() { cout << this->get_score() << "\n"; }
-	void print_name() { cout << this->get_name() << "\n"; }
 };
 
